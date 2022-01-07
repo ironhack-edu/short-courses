48d027e16f83d6f69fb5e05a8662d45daa9dc7da
Jorge Berrizbeitia <58472035+jorgeberrizbeitia@users.noreply.github.com>
2022-01-07 11:41:38 +0100

    fixed emoji bug. working on verifications.

--- script.js
+++ script.js
@@ -14,6 +14,21 @@ const counterInput = document.querySelector("#counter-input");
 
 const confettiInput = document.querySelector("#confetti-function-input");
 
+const emojiOneInputCheck = document.querySelector("#emoji-one-input-check");
+
+
+function isEmoji(str) {
+  console.log(str)
+  var ranges = [
+      '(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])' // U+1F680 to U+1F6FF
+  ];
+  if (str.match(ranges.join('|'))) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
 // DOM TO ALTER
 
 const circleButtons = document.querySelectorAll(".circle-btn");
@@ -22,15 +37,24 @@ const counters = document.querySelectorAll(".counter");
 // ACTIVITY 1
 
 emojiOneInput.addEventListener("change", () => {
+  // console.log(isEmoji(emojiOneInput.innerText))
+  // if (isEmoji(emojiOneInput.innerText)) {
+  //   circleButtons[0].innerText = emojiOneInput.value;
+  //   emojiOneInputCheck.innerText = "All good!"
+  //   emojiOneInputCheck.style.color = "lightgreen"
+  // }
+
+
   circleButtons[0].innerText = emojiOneInput.value;
+
 });
 
 emojiTwoInput.addEventListener("change", () => {
-  circleButtons[1].innerText = emojiOneInput.value;
+  circleButtons[1].innerText = emojiTwoInput.value;
 });
 
 emojiThreeInput.addEventListener("change", () => {
-  circleButtons[2].innerText = emojiOneInput.value;
+  circleButtons[2].innerText = emojiThreeInput.value;
 });
 
 // ACTIVITY 2
@@ -45,8 +69,8 @@ circleButtons.forEach((eachCircle) => {
 
 circleButtons.forEach((eachCircle) => {
   eachCircle.addEventListener("mouseleave", () => {
-    eachCircle.style.width = "5px";
-    eachCircle.style.height = "5px";
+    eachCircle.style.width = "60px";
+    eachCircle.style.height = "60px";
     eachCircle.style.backgroundColor = "white";
   });
 });
